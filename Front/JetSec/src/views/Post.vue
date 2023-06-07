d05918a158f571b4f6cb998c38c7132047e058dd
renan <renanbickdev@gmail.com>
2023-06-07 15:54:32 -0300

    FIX: Alterações na página e nas lógicas do script

--- Front/JetSec/src/views/Post.vue
+++ Front/JetSec/src/views/Post.vue
@@ -1,44 +1,53 @@
 
 <template>
   <div id="#app">
-    <div class="section">
-      <div class="header">
-
-      </div>
-      <div class="content">
-
-      </div>
-      <div class="author">
-
+    <div class="section ">
+      <h1 v-if="setErrorMsg">{{errorMessage}}</h1>
+      <div v-for="article in dbResponsePost">
+        <p class="tag is-rounded" v-html="article.category"/>
+        <h1 class="title" v-html="article.title"/>
+        <p class="subtitle" v-html="article.author"/>
+        <p v-html="article.created"/>
+        <p class="content has-text-justified" v-html="article.content"></p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 export default{
   props: ['slug'],
   name: 'post',
   data(){
     return{
       postSlug: '',
-      dbResponseKeys: [],
+      dbResponsePost: '',
+      errorMessage: '',
+      setErrorMsg: false,
     }
   },
   created(){
-
     this.postSlug = this.$router.currentRoute.params.slug
-    console.log(this.postSlug);
-    axios.get(`http://localhost:8000/api/article/${this.postSlug}`)
-        .then((response) =>{
-              this.dbResponseKeys = response.data.articles;
-              console.log(this.dbResponseKeys)
-
+    axios.get(`http://localhost:8000/api/article-page?search=${this.postSlug}`, )
+        .then((response) => {
+          this.dbResponsePost = response.data.article;
+          this.setErrorMsg = false;
+          this.titles = this.dbResponsePost.map((article) => {
+            return {
+              title: article.title,
+              content: article.content,
+              author: article.author,
+              category: article.category,
+              created : article.created_at,
+            }
+          });
         })
         .catch((error) =>{
-
+            this.setErrorMsg = true;
+            this.errorMessage = error;
         })
+
   },
   methods:{
 
